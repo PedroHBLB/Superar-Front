a4d2361b4b095f107b1b9e70b0ba27efa68f65ed
Pedro Henrique Barbosa Leite Barbieri <85046306+PedroHBLB@users.noreply.github.com>
2022-11-24 11:50:13 -0300

    Alterando api do axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const ipOfc = process.env.IP_OFC
-
 const api = axios.create({
   // baseURL: "http://192.168.11.79:8000",
-  baseURL: ipOfc,
-  //baseURL: `http://192.168.11.105:3000`
+  //baseURL: "177.190.201.227:3000",
+  baseURL: `http://192.168.11.105:3000`
 });
 
 export { api };
